593a2d446b9d0a545ad0ae358fb7565cb5fc5bef
Oliver Pearce <OliverPearce13@gmail.com>
2023-09-02 13:51:55 -0700

    added a textured item to minecraft

--- src/main/java/silv3red/kyra/ExampleMod.java
+++ src/main/java/silv3red/kyra/ExampleMod.java
@@ -1,11 +1,21 @@
 package silv3red.kyra;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class ExampleMod implements ModInitializer {
+
+	// an instance of our new item
+    public static final Item CUSTOM_ITEM =
+      Registry.register(Registry.ITEM, new Identifier("kyra", "custom_item"),
+        new Item(new FabricItemSettings()));
+
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
@@ -18,4 +28,4 @@ public class ExampleMod implements ModInitializer {
 
 		LOGGER.info("Hello Fabric world!");
 	}
-}
+} 

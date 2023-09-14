73f8aae1037659a0e1489c1c9eb377c6072b45ea
Oliver Pearce <OliverPearce13@gmail.com>
2023-09-14 15:57:47 -0700

    added food functionality

--- src/main/java/silv3red/kyra/ExampleMod.java
+++ src/main/java/silv3red/kyra/ExampleMod.java
@@ -2,7 +2,9 @@ package silv3red.kyra;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -12,9 +14,11 @@ import org.apache.logging.log4j.Logger;
 public class ExampleMod implements ModInitializer {
 
 	// an instance of our new item
-    public static final Item CUSTOM_ITEM =
-      Registry.register(Registry.ITEM, new Identifier("kyra", "ramen"),
-        new Item(new FabricItemSettings()));
+    public static final Item RAMEN =
+      Registry.register(Registry.ITEM, 
+	  	new Identifier("kyra", "ramen"),
+        new Item(new FabricItemSettings().group(ItemGroup.FOOD)
+			.food(new FoodComponent.Builder().hunger(6).saturationModifier(0.6f).build())));
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
